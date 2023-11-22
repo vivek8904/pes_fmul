2cdfd851e3df9005389db6eb5fcbcf77c12159c1
vivek8904 <130572147+vivek8904@users.noreply.github.com>
2023-11-22 11:52:13 +0530

    Update pes_fmul_tb.v

--- pes_fmul_tb.v
+++ pes_fmul_tb.v
@@ -1,86 +1,58 @@
+`timescale 1ns / 1ps
+
 module pes_fmul_tb;
 
-reg [3:0] Mplier,Mcand;
-wire [6:0] Product;
-reg CLK,St;
-wire Done;
-pes_fmul x1 (CLK, St, Mplier, Mcand, Product, Done);
+  reg clk;
+  reg reset;
+  reg signed [7:0] x_fraction;
+  reg signed [7:0] y_fraction;
+  wire signed [15:0] product_fraction;
 
-initial
-begin
-CLK=1;
-forever #5 CLK=~CLK;
-end
+  // Instantiate the module
+  pes_fmul uut (
+    .clk(clk),
+    .reset(reset),
+    .x_fraction(x_fraction),
+    .y_fraction(y_fraction),
+    .product_fraction(product_fraction)
+  );
 
-initial
-begin
-#20;
+  // Clock generation
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
 
+  // Initial stimulus
+  initial begin
+    $dumpfile("out.vcd");
+    $dumpvars(0, pes_fmul_tb);
 
-St=1;
-Mplier=255;
-Mcand=255;
-#10
-St=0;
-#100
+    // Test Case 1
+    reset = 1;
+    x_fraction = 8'b01010101;
+    y_fraction = 8'b00110011;
+    #10 reset = 0;
 
+    // Test Case 2
+    #100 x_fraction = 8'b11111111;
+    y_fraction = 8'b00000001;
 
-#20
+    // Test Case 3
+    #100 reset = 1;
+    x_fraction = 8'b10101010;
+    y_fraction = 8'b01010101;
+    #10 reset = 0;
 
-St=1;
-Mplier=128;
-Mcand=128;
-#10
-St=0;
-#100
+    // Test Case 4
+    #100 reset = 1;
+    x_fraction = 8'b00001111;
+    y_fraction = 8'b00001111;
+    #10 reset = 0;
 
-St=1;
-Mplier=128;
-Mcand=0;
-#10
-St=0;
-#100
+    // You can add more test cases here...
 
-St=1;
-Mplier=128;
-Mcand=1;
-#10
-St=0;
-#100
+    #200 $finish;
+  end
 
-St=1;
-Mplier=25;
-Mcand=5;
-#10
-St=0;
-#100
-
-St=1;
-Mplier=64;
-Mcand=64;
-#10
-St=0;
-#100
-
-St=1;
-Mplier=36;
-Mcand=36;
-#10
-St=0;
-#100
-
-St=1;
-Mplier=11;
-Mcand=33;
-#10
-St=0;
-#100
-
-St=1;
-Mplier=80;
-Mcand=10;
-#10
-St=0;
-#100;
-end
 endmodule
